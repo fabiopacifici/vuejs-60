2a824a9227f27dee2aecf1e491a6bd8d2520c55a
Fabio <mail@fabiopacifici.com>
2022-04-21 18:58:34 +0200

    add contents v-html and dynamic attr

---
+++ directive-v-html-dynamic-attr/app.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: '<span>this is a message</span>',
+    link: 'https://v2.vuejs.org/v2/guide/syntax.html#Shorthands',
+    attr: 'href'
+  },
+  methods: {}
+})
