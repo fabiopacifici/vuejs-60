8f1d9a4f0bbcd20480e73319c39014f730768398
Fabio <mail@fabiopacifici.com>
2022-04-21 18:58:53 +0200

    add contents v-for directive

---
+++ directive-v-for/app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    movies: [
+      { title: 'Avatar' },
+      { title: 'Matrix' },
+      { title: 'Ritorno al futuro' },
+      { title: 'Spiderman' },
+    ]
+  },
+  methods: {}
+})
