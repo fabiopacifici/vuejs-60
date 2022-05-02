e81f5ae574183c99cf6e2c7fe0ea45ff68bd4d79
Fabio <mail@fabiopacifici.com>
2022-05-02 17:33:13 +0200

    add mytest app with vue cli

---
+++ my_test_app/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h3>Ciao Ciao</h3>
+    <List/>
+
+    <h4>Another list</h4>
+    <ul>
+      <li>Fabio</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import List from "@/components/ListComponent.vue"
+
+export default {
+  name: 'App',
+  components: {
+    List
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my_test_app/src/components/ListComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li>Ciao 1</li>
+      <li>Ciao 2</li>
+      <li>Ciao 3</li>
+      <li>Ciao 4</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ListComponent'
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+
+}
+</style>
