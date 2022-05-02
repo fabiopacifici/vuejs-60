71bd41f5f00e83be3f75823aae3d72928b617a5c
Fabio <mail@fabiopacifici.com>
2022-05-02 17:32:39 +0200

    Add live ex email with axios

---
+++ live-ex-email/app.js
@@ -0,0 +1,30 @@
+/* Descrizione:
+  Attraverso l 'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'
+interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    error: null,
+    emailList: []
+  },
+  mounted() {
+
+
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+          console.log(response.data.response);
+          this.emailList.push(response.data.response)
+        })
+        .catch(error => {
+          console.error(error);
+          // this.error = error.message
+        })
+    }
+
+  }
+})
