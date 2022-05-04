e6a0c427ca33cb656f066aec64f1dd77fb8a874b
Fab <boolean@fabiopacifici.com>
2022-05-04 16:28:33 +0200

    Add ProductComponent with props la molisana

---
+++ lamolisana/src/components/ProductComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-3">
+    <div class="product">
+      <img :src="src" :alt="titolo" />
+      <h4>{{ titolo }}</h4>
+
+<!-- 
+       <img :src="pasta.src" :alt="pasta.titolo" />
+      <h4>{{ pasta.titolo }}</h4> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    nome: 'ProductComponent',
+    props: {
+        //pasta : Object,
+        src: String,
+        titolo: String
+    }
+
+};
+</script>
+
+<style lang="scss" scoped>
+.product {
+  margin-bottom: 1rem;
+  background-color: white;
+}
+</style>
