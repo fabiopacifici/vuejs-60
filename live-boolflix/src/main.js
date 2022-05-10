f3bf1eabf5630a98c253b2d99e91de3164bf48fd
Fabio <mail@fabiopacifici.com>
2022-05-10 23:55:42 +0200

    complete font-awesome installation

--- live-boolflix/src/main.js
+++ live-boolflix/src/main.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import LangFlag from 'vue-lang-code-flags';
+/* Font Awesome icons*/
+/* import the fontawesome core */
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import {
+  faUserSecret,
+  faMagnifyingGlass
+} from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(faStar)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* lang Flags component */
 Vue.component('lang-flag', LangFlag);
 
 Vue.config.productionTip = false
